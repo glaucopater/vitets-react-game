7c190f4b7b41397313b2dfe4249596e8ed760452
Glauco Pater <glauco.pater@gmail.com>
2024-04-28 19:31:37 +0200

    Bump version Update husky

--- src/components/Player/Player.test.tsx
+++ src/components/Player/Player.test.tsx
@@ -3,23 +3,23 @@ import { Player } from ".";
 import { getPlayerAvatar } from "../../helpers";
 
 it("renders Player component without crashing", () => {
-  render(<Player position={{ x: 0, y: 0 }} health={100} />);
+  render(<Player position={{ x: 0, y: 0 }} health={100} isPaused={false} />);
 });
 
 it("renders Player component with correct aria-label and role properties", () => {
-  render(<Player position={{ x: 0, y: 0 }} health={100} />);
+  render(<Player position={{ x: 0, y: 0 }} health={100} isPaused={false} />);
   const playerElement = screen.getByLabelText("player");
   expect(playerElement).toHaveAttribute("role", "img");
 });
 
 it("renders Player component with correct font size", () => {
-  render(<Player position={{ x: 0, y: 0 }} health={100} />);
+  render(<Player position={{ x: 0, y: 0 }} health={100} isPaused={false} />);
   const playerElement = screen.getByText(getPlayerAvatar(100));
   expect(playerElement).toHaveStyle({ fontSize: "30px" });
 });
 
 it("renders Player component with correct avatar when game is over", () => {
-  render(<Player position={{ x: 0, y: 0 }} health={0} />);
+  render(<Player position={{ x: 0, y: 0 }} health={0} isPaused={false} />);
   const playerElement = screen.getByText(getPlayerAvatar(0));
   expect(playerElement).toBeInTheDocument();
 });
